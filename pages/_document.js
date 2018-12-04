ea2d9bd7a3e6009dead62bf165dc2915e5ba5d50
Michael Tiller <michael.tiller@xogeny.com>
2018-12-04 11:21:17 -0500

    Added blueprint and associated CSS

---
+++ pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <html>
+                <Head>
+                    <link
+                        href="https://unpkg.com/@blueprintjs/icons@^3.0.0/lib/css/blueprint-icons.css"
+                        rel="stylesheet"
+                    />
+                </Head>
+                <body className="custom_class">
+                    <Main />
+                    <NextScript />
+                </body>
+            </html>
+        );
+    }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
 import React from "react";
+import { Button, Card, Elevation } from "@blueprintjs/core";
+import "./index.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+// import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
-export default () => <div>Welcome to Next.js with Blueprint.js!</div>;
+export default () => (
+    <div>
+        <h2>Welcome to Next.js with Blueprint.js!</h2>
+        <Card interactive={true} elevation={Elevation.TWO}>
+            <h5>
+                <a href="#">Card heading</a>
+            </h5>
+            <p>Card content</p>
+            <Button icon="refresh" />
+            <Button>Submit</Button>
+        </Card>
+    </div>
+);
