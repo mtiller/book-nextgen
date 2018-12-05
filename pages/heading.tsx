ee60d1a68eab8689be7c89baf48e2aa56cc109e4
Michael Tiller <michael.tiller@xogeny.com>
2018-12-05 09:43:43 -0500

    Got navigation added

---
+++ pages/heading.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Navbar, AnchorButton, Button } from "@blueprintjs/core";
+import { SphinxJsonData } from "./types";
+
+export const Heading2 = (props: any) => {
+    return (
+        <div>
+            <pre>{JSON.stringify(props, null, 4)}</pre>
+        </div>
+    );
+};
+export const Heading = (props: SphinxJsonData) => {
+    return (
+        <Navbar style={{ display: "flex", justifyContent: "space-between" }} fixedToTop={true}>
+            <Navbar.Group>
+                <Button className="bp3-minimal" icon="menu" text="Menu" />
+                {props.prev && (
+                    <AnchorButton
+                        className="bp3-minimal"
+                        icon="arrow-left"
+                        text={props.prev.title}
+                        href={props.prev.link}
+                    />
+                )}
+            </Navbar.Group>
+            <Navbar.Group>
+                <AnchorButton className="bp3-minimal" icon="home" text="Home" href="/" />
+            </Navbar.Group>
+            <Navbar.Group>
+                {props.next && (
+                    <AnchorButton
+                        className="bp3-minimal"
+                        rightIcon="arrow-right"
+                        text={props.next.title}
+                        href={props.next.link}
+                    />
+                )}
+            </Navbar.Group>
+        </Navbar>
+    );
+};
--- pages/pageview.tsx
+++ pages/pageview.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { SphinxJsonData } from "./types";
 import { getInitialProps } from "./data";
+import { Heading } from "./heading";
 
 export default class PageView extends React.Component<SphinxJsonData> {
     static getInitialProps = getInitialProps;
     render() {
+        const props = this.props;
         return (
             <div>
-                <div style={{ margin: 20 }}>
-                    <div dangerouslySetInnerHTML={{ __html: this.props.body }} />
+                <Heading {...props} />
+                <div style={{ width: "100%", display: "flex", justifyContent: "space-around", marginTop: "60px" }}>
+                    <img src="/static/images/TitleHeading.png" />
                 </div>
-                <pre>{JSON.stringify({ ...this.props, url: null }, null, 4)}</pre>
+
+                <div style={{ margin: 20 }}>
+                    <div dangerouslySetInnerHTML={{ __html: props.body }} />
+                </div>
+                <pre>{JSON.stringify({ ...props, url: null }, null, 4)}</pre>
             </div>
         );
     }
