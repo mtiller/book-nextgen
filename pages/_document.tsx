c3725a15c4943d6024dc9cae479d481a8f5d4fd7
Michael Tiller <michael.tiller@xogeny.com>
2019-06-03 19:45:27 -0400

    Updated analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -84,17 +84,17 @@ export default class MyDocument extends Document<PageData> {
                     <script async={true} src="//www.google-analytics.com/analytics.js" />
                     <script type="text/javascript" src="/static/jquery.js" />
 
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-33034217-6" />
+
                     <script
                         dangerouslySetInnerHTML={{
-                            __html: `(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-      m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-  })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-  ga('create', 'UA-33034217-6', 'xogeny.com');
-  ga('send', 'pageview');`,
+                            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                          
+                            gtag('config', 'UA-33034217-6');                          
+`,
                         }}
                     />
                 </Head>
